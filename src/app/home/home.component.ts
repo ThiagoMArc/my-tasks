ff7ecf473dbc26153ba23069967c5f6ce113a3b7
Thiago Felipe <tf.arcanjo@gmail.com>
2021-07-10 18:10:18 -0300

    Alteração do nome do título da página

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  pageTitle = 'Bem Vindo ao MyTasks!'
+
   constructor() { }
 
   ngOnInit(): void {
